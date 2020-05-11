cd8fd7371a6045cda679f50d23ef13658f43bc5b
jan <j_sten07@uni-muenster.de>
2020-05-11 17:20:12 +0200

    divide functions and loading / processing of data (not for distance calculations yet)

---
+++ process_data.R
@@ -0,0 +1,46 @@
+### load data
+GHSL_ESP_2014 <- raster("original/GHSL/GHS_BUILT_LDS2014_GLOBE_R2018A_54009_250_V2_0_17_4_ESP/GHS_BUILT_LDS2014_GLOBE_R2018A_54009_250_V2_0_17_4.tif")
+GHSL_ESP_1990 <- raster("original/GHSL/GHS_BUILT_LDS1990_GLOBE_R2018A_54009_250_V2_0_17_4_ESP/GHS_BUILT_LDS1990_GLOBE_R2018A_54009_250_V2_0_17_4.tif")
+GHSL_ESP_30m <- raster("original/GHSL/GHS_BUILT_LDSMT_GLOBE_R2018A_3857_30_V2_0_13_8_ESP/GHS_BUILT_LDSMT_GLOBE_R2018A_3857_30_V2_0_13_8.tif")
+GHSL_POL_2014 <- raster("original/GHSL/GHS_BUILT_LDS2014_GLOBE_R2018A_54009_250_V2_0_19_3_POL/GHS_BUILT_LDS2014_GLOBE_R2018A_54009_250_V2_0_19_3.tif")
+GHSL_POL_1990 <- raster("original/GHSL/GHS_BUILT_LDS1990_GLOBE_R2018A_54009_250_V2_0_19_3_POL/GHS_BUILT_LDS1990_GLOBE_R2018A_54009_250_V2_0_19_3.tif")
+GHSL_POL_30m <- raster("original/GHSL/GHS_BUILT_LDSMT_GLOBE_R2018A_3857_30_V2_0_14_7_POL/GHS_BUILT_LDSMT_GLOBE_R2018A_3857_30_V2_0_14_7.tif")
+sevilla_boundaries <- shapefile("created/GADM/Sevilla_boundaries.shp")
+krakow_boundaries <- shapefile("created/GADM/Krakow_boundaries.shp")
+dresden_boundaries <- shapefile("created/GADM/Dresden_boundaries.shp")
+
+GHSL_pop_ESP <- raster("original/GHSL/GHS_POP_E2000_GLOBE_R2019A_54009_250_V1_0_17_4_ESP/GHS_POP_E2000_GLOBE_R2019A_54009_250_V1_0_17_4.tif")
+GHSL_pop_POL <- raster("original/GHSL/GHS_POP_E2000_GLOBE_R2019A_54009_250_V1_0_19_3_POL/GHS_POP_E2000_GLOBE_R2019A_54009_250_V1_0_19_3.tif")
+
+slope_ESP <- raster("original/slope/EUD_CP-SLOP_2500015000-AA_ESP/EUD_CP-SLOP_2500015000-AA.tif")
+slope_GER <- raster("original/slope/EUD_CP-SLOP_4500035000-AA_GER/EUD_CP-SLOP_4500035000-AA.tif")
+slope_POL <- raster("original/slope/EUD_CP-SLOP_5500035000-AA_POL/EUD_CP-SLOP_5500035000-AA.tif")
+
+
+### process and save data
+change_Sevilla <- getChange(GHSL_ESP_1990, GHSL_ESP_2014, sevilla_boundaries, epsg = 32630, resolution = 250, threshold = 50)
+writeRaster(change_Sevilla, "created/inR/sevilla_change.tif", overwrite=T)
+change_Sevilla_30m <- getChangeFromMultitemp(GHSL_ESP_30m, sevilla_boundaries, 32630, resolution = 30)
+writeRaster(change_Sevilla_30m, "created/inR/sevilla_change_30_m.tif", overwrite=T)
+change_Krakow <- getChange(GHSL_POL_1990, GHSL_POL_2014, krakow_boundaries, epsg = 32634, resolution = 250, threshold = 50)
+writeRaster(change_Krakow, "created/inR/krakow_change.tif", overwrite=T)
+change_Krakow_30m <- getChangeFromMultitemp(GHSL_POL_30m, krakow_boundaries, 32634, resolution = 30)
+writeRaster(change_Krakow_30m, "created/inR/krakow_change_30_m.tif", overwrite=T)
+change_Dresden <- getChange(GHSL_POL_1990, GHSL_POL_2014, dresden_boundaries, epsg = 32633, resolution = 250, threshold = 50)
+writeRaster(change_Dresden, "created/inR/dresden_change.tif", overwrite=T)
+change_Dresden_30m <- getChangeFromMultitemp(GHSL_POL_30m, dresden_boundaries, 32633, resolution = 30)
+writeRaster(change_Dresden_30m, "created/inR/dresden_change_30_m.tif", overwrite=T)
+
+population_Sevilla <- reprojectAndCrop(GHSL_pop_ESP, sevilla_boundaries, 25830, 250)
+writeRaster(population_Sevilla, "created/inR/sevilla_popDens.tif", overwrite=T)
+population_Krakow <- reprojectAndCrop(GHSL_pop_POL, krakow_boundaries, 25834, 250)
+writeRaster(population_Krakow, "created/inR/krakow_popDens.tif", overwrite=T)
+population_Dresden <- reprojectAndCrop(GHSL_pop_POL, dresden_boundaries, 25833, 250)
+writeRaster(population_Dresden, "created/inR/dresden_popDens.tif", overwrite=T)
+
+slope_Sevilla <- citySlopeAsPercentage(slope_ESP, sevilla_boundaries, 25830)
+writeRaster(slope_Sevilla, "created/slope/sevilla_slope.tif", overwrite=T)
+slope_Dresden <- citySlopeAsPercentage(slope_GER, dresden_boundaries, 25833)
+writeRaster(slope_Dresden, "created/slope/dresden_slope.tif", overwrite=T)
+slope_Krakow <- citySlopeAsPercentage(slope_POL, krakow_boundaries, 25834)
+writeRaster(slope_Krakow, "created/slope/krakow_slope.tif", overwrite=T)
