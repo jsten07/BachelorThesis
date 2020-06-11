31fdfa14c2524e4a0871e426e2b99df8cb60af07
jan <j_sten07@uni-muenster.de>
2020-06-11 18:38:30 +0200

    add a ffs_plus model, which contains ffs coefficients plut landuse even if it not improves the model

--- regression.R
+++ regression.R
@@ -4,6 +4,7 @@ rm(list=ls())
 library(pROC)
 library(caret)
 library(varhandle)
+library(spdep) # morans
 
 setwd("C:/Users/janst/sciebo/Bachelor Thesis/data/created/samples/stratified/")
 
@@ -59,10 +60,12 @@ setwd("C:/Users/janst/sciebo/Bachelor Thesis/data/created/samples/stratified/")
 ###################### functions #######################################################################################
 ############################################################################################################################
 
-create_model <- function(data, significance = 0.05, split = T, train_part = 0.5, select = T) {
+create_model <- function(data, significance = 0.05, split = F, train_part = 0.5, select = T) {
   
   # split landuse in single binary variables
   # data.k.dummy <- cbind(data.k, to.dummy(data.k$landuse, prefix = "landuse"))
+  
+  data$landuse <- as.factor(data$landuse)
   
   # data <- split_landuse(data)
   
@@ -85,7 +88,11 @@ create_model <- function(data, significance = 0.05, split = T, train_part = 0.5,
   
   
   # 
-  formula <- "change ~"
+  formula <- "change ~ "
+  
+  if(TRUE %in% (summary(model)$coefficients[1:5,4] < significance)) {
+    formula <- paste(formula, "landuse")
+  }
   
   # find signifcant variables and add them to the formula for the new model
   for (i in c(6:length(summary(model)$coefficients[,4]))) {
@@ -93,6 +100,7 @@ create_model <- function(data, significance = 0.05, split = T, train_part = 0.5,
       formula <- paste(formula, "+", rownames(summary(model)$coefficients)[i])
     }
   }
+  
   
   # model with significant variables
   model.s <- glm(formula = formula,
@@ -128,6 +136,7 @@ calc_roc <- function(model, test_data = NULL) {
     
     print("model data")
   } else {
+    test_data$landuse <- as.factor(test_data$landuse)
     prob = predict(model, newdata = test_data, type = c("response"))
     # test_data$prob = prob
     g <- roc(test_data$change ~ prob, data = test_data)
@@ -151,7 +160,7 @@ calc_roc <- function(model, test_data = NULL) {
 # forward feature selection
 library(CAST)
 
-ffs_model <- function(data) {
+ffs_model <- function(data, significance = 0.5) {
 
   # split landuse 
   data.dummy <- data
@@ -163,36 +172,52 @@ ffs_model <- function(data) {
   ctrl <- trainControl(method = "cv", index = indices$index)
   
   # check for predictors (because landuse 3 is not existent in sevilla data)
-  predictors <- c("built_dens", "pop_dens", "slope", "mRoads_dist", "pRoads_dist", "river_dist", "train_dist", "center_dist", "airport_dist", "landuse")
+  predictors <- c("landuse", "built_dens", "pop_dens", "slope", "mRoads_dist", "pRoads_dist", "river_dist", "train_dist", "center_dist", "airport_dist")
   
   
+  # initialize trained object
   trained <- deparse(substitute(data))
   
-  # create model with all variables
-  # trained$glm <- glm(formula = change ~ factor(landuse) + built_dens + pop_dens + slope + 
-  #                mRoads_dist + pRoads_dist + river_dist + train_dist + center_dist + airport_dist,
-  #                family = "binomial",
-  #                data = data.dummy)
-  
+  # glm with all factors
   trained$glm <- create_model(data.dummy, split = F, select = F)
-  
-  trained$glm$sig <- create_model(data.dummy, split = F)
+
+  # glm with significant factors  
+  trained$glm$sig <- create_model(data.dummy, split = F, significance = significance)
   
   trained$cv_strata <- cv_strata_l
   
+  # use forward feature selection to select best combination of determinants
   trained$ffs <- ffs(data.dummy[,predictors], data.dummy[,"change"],
                    method = "glm",
-                   family = "binomial",
+                   family = "binomial", 
                    trControl = ctrl)
   
+  # create a model with cv containing all determinants
   trained$train <- train(data.dummy[,predictors], data.dummy[,"change"],
-                     method = "glm",
-                     family = "binomial",
-                     trControl = ctrl)
+                         method = "glm",
+                         family = "binomial",
+                         trControl = ctrl)
+  
+  # if a ffs model doesnt contain landuse, calculate a model with landuse
+  ffs_predictors <- names(trained$ffs$finalModel$coefficients)
+  if(!("landuse2" %in% ffs_predictors)) {
+    ffs_predictors <- append(ffs_predictors, "landuse")
+    ffs_predictors <- ffs_predictors[-1]
+    trained$ffs_plus <- train(data.dummy[,ffs_predictors], data.dummy[,"change"],
+                           method = "glm",
+                           family = "binomial",
+                           trControl = ctrl)
+  } else {
+    trained$ffs_plus <- trained$ffs
+  }
+   
+  
+  untrace(caret::createModel)
   
   return(trained)
   
 }
+
 
 
 # trained.s <- ffs_model(data.s)
@@ -234,6 +259,57 @@ split_landuse <- function(data) {
   
   return(data.dummy)
 }
+
+
+
+########################################################################################################
+# Moran's I
+########################################################################################################
+
+
+calc_moransI <- function(samples, dist = NULL, k = NULL) {
+  
+  # get coordinates as matrix
+  coords <- as.matrix(cbind(samples$x, samples$y))
+  
+  if(!is.null(dist)) {
+    
+    nb <- dnearneigh(coords, 0, dist)
+    
+  } else if(!is.null(k)) {
+    
+    nb <- knn2nb(knearneigh(coords, k = k))
+    
+  } else {
+    
+    # identify neighbours
+    # within the minimum distance so every sample has at least one neighbour
+    k1 <- knn2nb(knearneigh(coords))
+    k1dists <- unlist(nbdists(k1, coords))
+    # summary(k1dists)
+    nb <- dnearneigh(coords, 0, max(k1dists))
+    print("max dist:")
+    print(max(k1dists))
+    
+  }
+  
+  print(nb)
+  
+  # get neighbour list
+  lw <- nb2listw(nb,zero.policy = T)
+  
+  # lw_k1 <- nb2listw(k1, zero.policy = T)
+  
+  # calculate Morans I
+  morans <- moran.test(samples$change, lw, randomisation = F, alternative = "two.sided", zero.policy = T)
+  
+  return(morans)
+}
+
+
+
+
+
 # train model
 # 
 # predictors <- c("change", "built_dens", "pop_dens", "slope", "landuse", "mRoads_dist", "pRoads_dist", "river_dist", "train_dist", "center_dist", "airport_dist", "cv_strata")
